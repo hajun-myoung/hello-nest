8a6464ce813b8a00829fef53f7c04081088652f6
Denver <audgkwns44@gmail.com>
2024-03-13 14:34:39 +0900

    [Create]More Methods

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -10,5 +10,20 @@ export class MoviesController {
   @Get('/:id')
   getOneById(@Param('id') id: string) {
     return `This will return one move. ID: ${id}`;
+  }
+
+  @Post()
+  createMovie() {
+    return 'this will create movie';
+  }
+
+  @Delete('/:id')
+  deleteMovie(@Param('id') id: string) {
+    return `this will delete a movie. ID: ${id}`;
+  }
+
+  @Patch('/:id')
+  patchMovie(@Param('id') id: string) {
+    return `this will update a movie. ID: ${id}`;
   }
 }
