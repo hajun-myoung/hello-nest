84d640209a6465560f87d4f9bac4c89adce47381
Denver <audgkwns44@gmail.com>
2024-03-14 13:47:58 +0900

    [Craete]String, number validator pipe

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,12 @@
+import { IsNumber, IsString, isString } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsString()
   readonly title: string;
+
+  @IsNumber()
   readonly year: number;
+
+  @IsString({ each: true })
   readonly genres: string[];
 }
